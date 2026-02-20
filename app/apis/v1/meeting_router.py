eadaf3918afd8272cff3e79e8480b4a667d838f2
Mashiro2465 <rlaalstjr2465@gmail.com>
2026-02-20 11:21:54 +0900

    install tortoise

--- app/apis/v1/meeting_router.py
+++ app/apis/v1/meeting_router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from app.dtos.create_meeting_response import CreateMeetingResponse
+from app.service.meeting_service_mysql import service_create_meeting_mysql
 
 edgedb_router = APIRouter(prefix="/v1/edgedb/meetings", tags=["Meeting"], redirect_slashes=False)
 mysql_router = APIRouter(prefix="/v1/mysql/meetings", tags=["Meeting"], redirect_slashes=False)
@@ -14,9 +15,6 @@ async def api_create_meeting_edgedb() -> CreateMeetingResponse:
     return CreateMeetingResponse(url_code="abc")
 
 
-@mysql_router.post(
-    "",
-    description="meeting 을 생성합니다.",
-)
+@mysql_router.post("", description="meeting 을 생성합니다.")
 async def api_create_meeting_mysql() -> CreateMeetingResponse:
-    return CreateMeetingResponse(url_code="abc")
+    return CreateMeetingResponse(url_code=(await service_create_meeting_mysql()).url_code)
